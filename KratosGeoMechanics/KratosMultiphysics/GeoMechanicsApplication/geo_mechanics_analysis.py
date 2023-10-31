879177e5bb0211df7295ddf64694f44ed1a56995
noordam <aronnoordam@gmail.com>
2023-10-31 17:37:33 +0100

    added wheels

--- KratosGeoMechanics/KratosMultiphysics/GeoMechanicsApplication/geo_mechanics_analysis.py
+++
@@ -1,6 +0,0 @@
-from KratosMultiphysics.GeoMechanicsApplication import geomechanics_analysis
-
-class GeoMechanicsAnalysis(geomechanics_analysis.GeoMechanicsAnalysis):
-
-    def __init__(self, model, project_parameters):
-        super().__init__(model, project_parameters)
