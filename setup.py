6ce511166ee283c9585d6839655f923b811e8a60
mcgicjn2 <jonathan.nuttall@deltares.nl>
2023-11-13 13:43:21 +0100

    Added new approach to moving the necessary python files

--- setup.py
+++ setup.py
@@ -1,4 +1,25 @@
 from setuptools import setup
+from setuptools.command.install import install
+import os
+import shutil
+
+class CustomStemInstallCommand(install):
+    def run(self):
+        # Call the default install process
+        install.run(self)
+
+        # Custom logic to move data from my_package to another_package
+        source_path = 'StemKratos/StemApplication'
+        destination_path = 'KratosMultiphysics/StemApplication'
+
+        # Ensure the destination directory exists
+        os.makedirs(destination_path, exist_ok=True)
+
+        # Move the entire directory
+        shutil.move(source_path, destination_path)
 
 if __name__ == '__main__':
-    setup()
+    setup(
+    cmdclass={
+        'install': CustomInstallCommand,
+    },
