6b47ef35573c23639ffb7428aa272e856a982aa6
noordam <aronnoordam@gmail.com>
2023-11-24 14:02:02 +0100

    reverted geomechanics_analysis.py

--- StemKratos/StemApplication/geomechanics_analysis.py
+++ StemKratos/StemApplication/geomechanics_analysis.py
@@ -12,33 +12,26 @@ class StemGeoMechanicsAnalysis(GeoMechanicsAnalysis):
     def _CreateSolver(self):
         return CreateSolver(self.model, self.project_parameters)
 
-if __name__ == '__main__':
-    # from sys import argv
-    #
-    # if len(argv) > 2:
-    #     err_msg =  'Too many input arguments!\n'
-    #     err_msg += 'Use this script in the following way:\n'
-    #     err_msg += '- With default parameter file (assumed to be called "ProjectParameters.json"):\n'
-    #     err_msg += '    "python geomechanics_analysis.py"\n'
-    #     err_msg += '- With custom parameter file:\n'
-    #     err_msg += '    "python geomechanics_analysis.py <my-parameter-file>.json"\n'
-    #     raise Exception(err_msg)
-    #
-    # if len(argv) == 2: # ProjectParameters is being passed from outside
-    #     parameter_file_name = argv[1]
-    # else: # using default name
-    #     parameter_file_name = "ProjectParameters.json"
-    #
-    # with open(parameter_file_name,'r') as parameter_file:
-    #     parameters = Kratos.Parameters(parameter_file.read())
-    #
-    import os
-    os.chdir(r"D:\software_development\STEM\benchmark_tests\benchmark_moving_load")
 
-    parameter_file_name = "ProjectParameters_stage_1.json"
+if __name__ == '__main__':
+    from sys import argv
+
+    if len(argv) > 2:
+        err_msg =  'Too many input arguments!\n'
+        err_msg += 'Use this script in the following way:\n'
+        err_msg += '- With default parameter file (assumed to be called "ProjectParameters.json"):\n'
+        err_msg += '    "python geomechanics_analysis.py"\n'
+        err_msg += '- With custom parameter file:\n'
+        err_msg += '    "python geomechanics_analysis.py <my-parameter-file>.json"\n'
+        raise Exception(err_msg)
+
+    if len(argv) == 2: # ProjectParameters is being passed from outside
+        parameter_file_name = argv[1]
+    else: # using default name
+        parameter_file_name = "ProjectParameters.json"
+
     with open(parameter_file_name,'r') as parameter_file:
         parameters = Kratos.Parameters(parameter_file.read())
-
 
     model = Kratos.Model()
     simulation = StemGeoMechanicsAnalysis(model, parameters)
