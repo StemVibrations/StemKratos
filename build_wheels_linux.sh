8effc128217dd2e8185b2387c3d5960f85f841fd
Bruno Zuada Coelho <brunozuada@gmail.com>
2024-01-24 16:08:57 +0100

    fixed wheels linux build

--- build_wheels_linux.sh
+++ build_wheels_linux.sh
@@ -7,5 +7,8 @@ docker build -t stemkratos:1.0 -f ./wheels_linux/Dockerfile ./wheels_linux
 # Run docker image
 docker run stemkratos:1.0
 
+# get docker id
+container_id=$(docker ps -q -a --filter "ancestor=stemkratos:1.0")
+
 # copy wheels to host
-docker cp stemkratos:1.0/data_swap_guest ./wheels_linux
+docker cp "${container_id}:/data_swap_guest" ./wheels_linux/wheels
