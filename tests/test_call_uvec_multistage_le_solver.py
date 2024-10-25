dd61f6883e7f68ba2209b50cdd302c5465a06f39
aronnoordam <aronnoordam@gmail.com>
2024-10-25 15:38:16 +0200

    added tests for multistage with linear elastic solver

--- tests/test_call_uvec_multistage_le_solver.py
+++ tests/test_call_uvec_multistage_le_solver.py
@@ -1,19 +1,12 @@
 import os
 from pathlib import Path
 
-import pytest
-import sys
-sys.path.append(r"C:\software_development\Kratos\bin\Release")
-sys.path.append(r"C:\software_development\Kratos\bin\Release\libs")
-import KratosMultiphysics as Kratos
-import KratosMultiphysics.StemApplication.geomechanics_analysis as analysis
-
 from tests.utils import assert_files_equal, assert_floats_in_files_almost_equal, Utils
 
 
 def test_call_uvec_multi_stage_le_solver():
     """
-    Test the call of the UVEC in a multi-stage analysis
+    Test the call of the UVEC in a multi-stage analysis, using the linear elastic solver.
     """
     test_file_dir = r"tests/test_data/input_data_multi_stage_uvec_le_solver"
 
@@ -23,8 +16,8 @@ def test_call_uvec_multi_stage_le_solver():
     Utils.run_multiple_stages(test_file_dir, project_parameters)
 
     # calculated disp below first wheel
-    calculated_disp_file = Path(r"tests/test_data/input_data_multi_stage_uvec/output/calculated_disp")
-    expected_disp_file = Path(r"tests/test_data/input_data_multi_stage_uvec/_output/expected_disp")
+    calculated_disp_file = Path(r"tests/test_data/input_data_multi_stage_uvec_le_solver/output/calculated_disp")
+    expected_disp_file = Path(r"tests/test_data/input_data_multi_stage_uvec_le_solver/_output/expected_disp")
 
     # check if calculated disp below first wheel is equal to expected disp
     are_files_equal, message = assert_floats_in_files_almost_equal(calculated_disp_file, expected_disp_file)
@@ -33,10 +26,10 @@ def test_call_uvec_multi_stage_le_solver():
     calculated_disp_file.unlink()
     assert are_files_equal, message
 
-    expected_vtk_output_dir = Path("tests/test_data/input_data_multi_stage_uvec/_output/all")
+    expected_vtk_output_dir = Path("tests/test_data/input_data_multi_stage_uvec_le_solver/_output/all")
 
-    main_vtk_output_dir = Path("tests/test_data/input_data_multi_stage_uvec/output/porous_computational_model_part_1")
-    stage_vtk_output_dir = Path("tests/test_data/input_data_multi_stage_uvec/output/porous_computational_model_part_2")
+    main_vtk_output_dir = Path("tests/test_data/input_data_multi_stage_uvec_le_solver/output/porous_computational_model_part_1")
+    stage_vtk_output_dir = Path("tests/test_data/input_data_multi_stage_uvec_le_solver/output/porous_computational_model_part_2")
 
     # move all vtk files in stage vtk output dir to main vtk output dir
     for file in os.listdir(stage_vtk_output_dir):
@@ -49,52 +42,3 @@ def test_call_uvec_multi_stage_le_solver():
 
     # check if vtk files are equal
     assert assert_files_equal(expected_vtk_output_dir, main_vtk_output_dir)
-
-
-def test_call_uvec_multi_stage_expected_fail():
-    """
-    Test the call of the UVEC in a multi-stage analysis. This test is expected to fail, as extra DOFS are added in the
-    second stage
-    """
-    test_file_dir = r"tests/test_data/input_data_multi_stage_uvec"
-
-    project_parameters = ["ProjectParameters_stage1.json", "ProjectParameters_stage2.json"]
-
-    cwd = os.getcwd()
-
-    # initialize model
-    model = Kratos.Model()
-
-    # read first stage parameters
-    os.chdir(test_file_dir)
-    with open(project_parameters[0], 'r') as parameter_file:
-        parameters = Kratos.Parameters(parameter_file.read())
-
-    # remove rotation dofs from first stage
-    parameters["solver_settings"]["rotation_dofs"].SetBool(False)
-
-    # create and run first stage
-    stage = analysis.StemGeoMechanicsAnalysis(model, parameters)
-    stage.Run()
-
-    # read second stage parameters
-    with open(project_parameters[1], 'r') as parameter_file:
-        parameters = Kratos.Parameters(parameter_file.read())
-
-    # make sure rotation dofs are added in second stage
-    parameters["solver_settings"]["rotation_dofs"].SetBool(True)
-
-    # create second stage with expected fail
-    with pytest.raises(RuntimeError) as excinfo:
-        analysis.StemGeoMechanicsAnalysis(model, parameters)
-
-    # check if the error message is as expected
-    assert ('Error: Attempting to add the variable "ROTATION" to the model part with name "PorousDomain"'
-            in str(excinfo.value))
-
-    # change working directory back to original working directory
-    os.chdir(cwd)
-
-    # remove uvec disp file
-    calculated_disp_file = Path(r"tests/test_data/input_data_multi_stage_uvec/output/calculated_disp")
-    calculated_disp_file.unlink()
