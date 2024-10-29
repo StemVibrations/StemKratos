1c159401dbae0182bcdb65ba89d34745102fe0ab
brunozc <brunozuada@gmail.com>
2024-10-29 13:12:52 +0100

    Update version and branch for Linux wheels

--- build_wheels_linux.sh
+++ build_wheels_linux.sh
@@ -1,5 +1,5 @@
-NEW_VERSION="9.4.3.3"
-NEW_BRANCH="geo/stem_intermediate_whls"
+NEW_VERSION="9.5.0.4"
+NEW_BRANCH="geo/linear_elastic_strategy"
 
 # Update the build.sh file with version
 sed -i "s/^\(export KRATOS_VERSION=\).*$/\1\"$NEW_VERSION\"/" wheels_linux/build.sh
@@ -11,13 +11,13 @@ sed -i "s#geo/stem_branch#$NEW_BRANCH#" wheels_linux/Dockerfile
 sudo service docker start
 
 # Build docker image
-docker build -t stemkratos:1.0 -f ./wheels_linux/Dockerfile ./wheels_linux
+docker build -t stemkratos:1.2 -f ./wheels_linux/Dockerfile ./wheels_linux
 
 # Run docker image
-docker run stemkratos:1.0
+docker run -v $(pwd)/wheels_linux/wheels:/data_swap_guest stemkratos:1.2
 
 # get docker id
-container_id=$(docker ps -q -a --filter "ancestor=stemkratos:1.0")
+container_id=$(docker ps -q -a --filter "ancestor=stemkratos:1.2")
 
 # copy wheels to host
 docker cp "${container_id}:/data_swap_guest" ./wheels_linux/wheels
