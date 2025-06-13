6fa244083335c70a7ffc3b450360078aee8021d7
aronnoordam <aronnoordam@gmail.com>
2025-06-13 15:41:02 +0200

    displacements are now not set to 0 at each stage

--- StemKratos/StemApplication/geomechanics_analysis.py
+++ StemKratos/StemApplication/geomechanics_analysis.py
@@ -1,5 +1,7 @@
 import KratosMultiphysics as Kratos
+import KratosMultiphysics.GeoMechanicsApplication as KratosGeo
 
+from KratosMultiphysics.analysis_stage import AnalysisStage
 from KratosMultiphysics.GeoMechanicsApplication.geomechanics_analysis import GeoMechanicsAnalysis
 from KratosMultiphysics.StemApplication.geomechanics_solvers_wrapper import CreateSolver
 
@@ -8,6 +10,32 @@ class StemGeoMechanicsAnalysis(GeoMechanicsAnalysis):
 
     def __init__(self, model, project_parameters):
         super().__init__(model, project_parameters)
+
+    def Initialize(self):
+        """
+        Initialize stage. This runs initialize AnalysisStage instead of GeoMechanicsAnalysis.Initialize. Such that
+        displacements are not set to 0.
+        """
+
+        # Run initalize AnalysisStage instead of GeoMechanicsAnalysis.Initialize
+        AnalysisStage.Initialize(self)
+
+        # In GeoMechanicsAnalysis, DISPLACEMENT and ROTATION will be set to zero every stage. This prevents
+        # this behavior in the STEM application. Instead the constitutive law will be reset at the end of each stage.
+        self._GetSolver().main_model_part.ProcessInfo[KratosGeo.RESET_DISPLACEMENTS] = self.reset_displacements
+        if self.reset_displacements:
+            self.ResetIfHasNodalSolutionStepVariable(KratosGeo.TOTAL_DISPLACEMENT)
+            Kratos.VariableUtils().UpdateCurrentToInitialConfiguration(
+                self._GetSolver().GetComputingModelPart().Nodes)
+
+    def Finalize(self):
+        """
+        Finalize stage and reset constitutive law of each element
+        """
+        super().Finalize()
+
+        for element in self._GetSolver().GetComputingModelPart().Elements:
+            element.ResetConstitutiveLaw()
 
     def _CreateSolver(self):
         return CreateSolver(self.model, self.project_parameters)
